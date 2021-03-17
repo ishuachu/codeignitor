9221435bdfc747560498183b5aa383ae0523ea74
ishuachu <43605475+ishuachu@users.noreply.github.com>
2021-03-17 17:26:06 +0530

    registration and login process

---
+++ login.php
@@ -0,0 +1,11 @@
+<center>
+<h2>login page</h2>
+<form action="login_process" method="post">
+<!--admin id:  <input type="text" name="empid" value='%s' readonly><br><br-->
+Email     :  <input type="mailid" name="email" required><br><br>
+Password  :  <input type="password" name="password" required><br><br>
+
+<input type="submit" name="login_submit" value="submit">
+<input type="reset" value="clear">
+</form>
+</center>
---
+++ registration.php
@@ -0,0 +1,14 @@
+
+<center>
+<h2>registration</h2>
+<form action="registration_process" method="post">
+<!--admin id:  <input type="text" name="empid" value='%s' readonly><br><br-->
+Name      :  <input type="text" name="name" required><br><br>
+Email     :  <input type="mailid" name="email" required><br><br>
+Mobile    :  <input type="number" name="mobile" required><br><br>
+Password  :  <input type="password" name="password" required><br><br>
+
+<input type="submit" name="registration_submit" value="save">
+<input type="reset" value="clear">
+</form>
+</center>
